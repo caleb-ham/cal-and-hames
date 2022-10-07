53563733c86d16c9f466709b73b8bc539fac707c
ja244121 <ja244121@falmouth.ac.uk>
2022-10-07 13:13:11 +0100

    Ben Made Code Work

--- braitenburg/braitenburg.ino
+++ braitenburg/braitenburg.ino
@@ -1,11 +1,11 @@
 #include "Servo.h"
- 
+
 Servo leftServo;
 Servo rightServo;
 int leftValue = 0;
 int rightValue = 0;
 
-const int ldrPinLeft = A2; 
+const int ldrPinLeft = A2;
 const int ldrPinRight = A1;
 
 
@@ -16,7 +16,7 @@ void setup() {
   pinMode(ldrPinLeft, INPUT);
   pinMode(ldrPinRight, INPUT);
 }
- 
+
 void loop() {
 
   //Serial.print("QA");
@@ -29,27 +29,63 @@ void loop() {
   //leftServo.write(89 + rightValue);
   //rightServo.write(89 - leftValue);
   delay(10);
-
+  Serial.print(ldrStatusLeft);
+  Serial.print(',');
+  Serial.println(ldrStatusRight);
+  if (ldrStatusLeft <300){
+    Serial.println("Dark");
+    leftServo.write(90);  
+    delay(100);
+  }
+  else if (ldrStatusLeft >600){
+    Serial.println("SUPER LIGHT");
+    leftServo.write(0);
+    delay(100);
+  }
+  else{
+    Serial.println("Light");
+    leftServo.write(45);
+    delay(100);
+  }
+  if (ldrStatusRight <300){
+    Serial.println("Dark");
+    rightServo.write(90);  
+    delay(100);
+  }
+  else if (ldrStatusRight >600){
+    Serial.println("SUPER LIGHT");
+    rightServo.write(180);
+    delay(100);
+  }
+  else{
+    Serial.println("Light");
+    rightServo.write(135);
+    delay(100);
+  }
   ldrStatusLeft = map(ldrStatusLeft, 100, 900, 0, 10);
   ldrStatusRight = map(ldrStatusRight, 100, 900, 0, 10);
-  
-  Serial.print(ldrStatusLeft);
-  Serial.print(",");
-  Serial.print(ldrStatusRight);
-  Serial.println();
+
+  //Serial.print(ldrStatusLeft);
+  //Serial.print(",");
+  //Serial.print(ldrStatusRight);
+  //Serial.println();
 
 
 
-  if (ldrStatusLeft > 2) {
-      leftServo.write(180 * ldrStatusLeft/10);
-      Serial.print(ldrStatusLeft);
-      Serial.print(",");
+  /*if (ldrStatusLeft > 2) {
+    leftServo.write(10 * ldrStatusLeft / 10);
+     Serial.print(ldrStatusLeft);
+     Serial.print(",");
+  } else {
+    leftServo.write(0);
   }
   if (ldrStatusRight > 2) {
-      rightServo.write(180 * ldrStatusRight/10);
-      Serial.print(ldrStatusRight);
-
-  Serial.println();
-
+    rightServo.write(10 * ldrStatusRight / 10);
+    // Serial.print(ldrStatusRight);
+  } else {
+    rightServo.write(0);
   }
+  Serial.println();
+  delay(100);
+  */  
 }
